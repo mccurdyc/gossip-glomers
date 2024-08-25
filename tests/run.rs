4e9886febb252f5b7773b9332f8f5c3488f5ba7a
Colton J. McCurdy <mccurdyc22@gmail.com>
2024-08-25 17:33:50 -0400

    table-driven tests
    Signed-off-by: Colton J. McCurdy <mccurdyc22@gmail.com>

--- tests/run.rs
+++ tests/run.rs
@@ -21,57 +21,37 @@ mod tests {
     }
 
     #[test]
-    fn init() {
-        let input = r#"{
-    "src": "c1",
-    "dest": "n1",
-    "body": {
-        "type": "init",
-        "msg_id": 1,
-        "node_id": "n3",
-        "node_ids": ["n1", "n2", "n3"]
-    }
-}"#;
-
-        let expected = r#"{"src":"n1","dest":"c1","body":{"type":"init_ok","in_reply_to":1}}
-"#;
-
-        // Necessary to implement Read trait on BufReader for bytes
-        let mut vec: Vec<u8> = Vec::new();
-        let mut write_cursor = Cursor::new(&mut vec);
-        let read_cursor = Cursor::new(input.as_bytes());
-
-        echo::listen(read_cursor, &mut write_cursor).expect("listen failed");
-
-        assert_eq!(String::from_utf8(vec).unwrap(), expected);
-    }
-
-    #[test]
     fn echo() {
-        use std::io::Cursor;
-        use std::vec::Vec;
-        setup();
+        let test_cases = vec![
+            (
+                r#"{"src":"c1","dest":"n1","body":{"type":"init","msg_id":1,"node_id":"n3","node_ids":["n1", "n2", "n3"]}}
+"#,
+                r#"{"src":"n1","dest":"c1","body":{"type":"init_ok","in_reply_to":1}}
+"#,
+            ),
+            (
+                r#"{"src":"c1","dest":"n1","body":{"type":"echo","msg_id":1,"echo":"Please echo 35"}}
+"#,
+                r#"{"src":"n1","dest":"c1","body":{"type":"echo_ok","msg_id":1,"in_reply_to":1,"echo":"Please echo 35"}}
+"#,
+            ),
+            (
+                r#"{"src":"f11","dest":"z10","body":{"type":"echo","msg_id":99,"echo":"Please echo 99"}}
+"#,
+                r#"{"src":"z10","dest":"f11","body":{"type":"echo_ok","msg_id":99,"in_reply_to":99,"echo":"Please echo 99"}}
+"#,
+            ),
+        ];
 
-        let input = r#"{
-    "src": "c1",
-    "dest": "n1",
-    "body": {
-        "type": "echo",
-        "msg_id": 1,
-        "echo": "Please echo 35"
-    }
-}"#;
+        for (input, expected) in test_cases {
+            // Necessary to implement Read trait on BufReader for bytes
+            let mut vec: Vec<u8> = Vec::new();
+            let mut write_cursor = Cursor::new(&mut vec);
+            let read_cursor = Cursor::new(input.as_bytes());
 
-        let expected = r#"{"src":"n1","dest":"c1","body":{"type":"echo_ok","msg_id":1,"in_reply_to":1,"echo":"Please echo 35"}}
-"#;
+            echo::listen(read_cursor, &mut write_cursor).expect("listen failed");
 
-        // Necessary to implement Read trait on BufReader for bytes
-        let mut vec: Vec<u8> = Vec::new();
-        let mut write_cursor = Cursor::new(&mut vec);
-        let read_cursor = Cursor::new(input.as_bytes());
-
-        echo::listen(read_cursor, &mut write_cursor).expect("listen failed");
-
-        assert_eq!(String::from_utf8(vec).unwrap(), expected);
+            assert_eq!(String::from_utf8(vec).unwrap(), expected);
+        }
     }
 }
